2c95674566d1afd16bd51ea1809eb1089dc87e9b
Douglas Magno <domagn2@gmail.com>
2021-09-21 20:41:59 -0300

    refactor: update service with new product models

--- _shared/service.py
+++ _shared/service.py
@@ -4,13 +4,22 @@ import requests
 
 from parsers.americanas import Americanas
 from parsers.magalu import Magalu
-from .product import Product
+from .models import Product
 from .scraper import Scraper
 
 
 class Service:
 
+    def find_product_by_url(self, url) -> Product:
+        try:
+            return Product.objects(url=url).first()
+        except:
+            return None
+
     def get_product_data(self, url: str):
+        product_in_data_base = self.find_product_by_url(url)
+        if product_in_data_base:
+            return product_in_data_base.to_dict()
         product = Product(url=url)
         parser = self.choose_parser(url)
         scraper = Scraper(product, parser)
