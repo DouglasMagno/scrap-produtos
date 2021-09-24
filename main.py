91c7b3803d7d67c229024163b9009796dabc2098
Douglas Magno <domagn2@gmail.com>
2021-09-23 21:53:14 -0300

    refactor: adjusts main

--- main.py
+++ main.py
@@ -1,12 +1,13 @@
 import os
 
+from src._shared.constants import ENV
 from src._shared.server import Server
 from src._shared.service import Service
 
 if __name__ == '__main__':
     service = Service()
-    s = Server('bcb-ptax', service)
-    port = int(os.environ.get('PORT', 5000))
+    s = Server('scrap-produtos', service)
+    port = int(ENV.FLASK_PORT)
     s.server.app.run(
         host='0.0.0.0',
         port=port,
